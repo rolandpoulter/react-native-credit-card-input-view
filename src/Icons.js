421ce55dbd77882fd27142d74bd32d4afbdcd60d
Roland Poulter <rolandpoulter@gmail.com>
2021-07-05 17:54:02 -0700

    fix images

--- src/Icons.js
+++ src/Icons.js
@@ -1,16 +1,16 @@
 const Icons = {
-  cvc: require('./icons/stp_card_cvc.png'),
-  cvc_amex: require('./icons/stp_card_cvc_amex.png'),
-  'american-express': require('./icons/stp_card_amex.png'),
-  'diners-club': require('./icons/stp_card_diners.png'),
-  'master-card': require('./icons/stp_card_mastercard.png'),
-  maestro: require('./icons/stp_card_maestro.png'),
-  rupay: require('./icons/stp_card_rupay.png'),
-  mastercard: require('./icons/stp_card_mastercard.png'),
-  discover: require('./icons/stp_card_discover.png'),
-  jcb: require('./icons/stp_card_jcb.png'),
-  placeholder: require('./icons/stp_card_unknown.png'),
-  visa: require('./icons/stp_card_visa.png'),
+  cvc: require('./icons/stp_card_cvc.png').default,
+  cvc_amex: require('./icons/stp_card_cvc_amex.png').default,
+  'american-express': require('./icons/stp_card_amex.png').default,
+  'diners-club': require('./icons/stp_card_diners.png').default,
+  'master-card': require('./icons/stp_card_mastercard.png').default,
+  maestro: require('./icons/stp_card_maestro.png').default,
+  rupay: require('./icons/stp_card_rupay.png').default,
+  mastercard: require('./icons/stp_card_mastercard.png').default,
+  discover: require('./icons/stp_card_discover.png').default,
+  jcb: require('./icons/stp_card_jcb.png').default,
+  placeholder: require('./icons/stp_card_unknown.png').default,
+  visa: require('./icons/stp_card_visa.png').default,
 }
 
 export default Icons
